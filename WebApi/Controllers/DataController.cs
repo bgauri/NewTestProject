b3d42a68770a30bce658463d03211b7bfde7b769
gauri.bhide <gauri.bhide@PUNHJW380945D>
2017-09-06 13:52:59 +0530

    Add project files.

---
+++ WebApi/Controllers/DataController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class DataController : ApiController
+    {
+        public string Get()
+        {
+            return "Welcome to web api";
+        }
+        public List<string>Get(string id)
+        {
+            return new List<string>
+            {
+                "Data1",
+                "Data2"
+            };
+        }
+    }
+}
